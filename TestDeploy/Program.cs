25db09c0c2d208378d83164dfb724ad2dfdc206a
vaibhav-jadhav96 <vaibhavjadhav3489>
2025-07-29 08:07:08 +0530

    Add project files.

---
+++ TestDeploy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDeploy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test t=new Test();  
+            t.TestCode();   
+        }
+    }
+}
---
+++ TestDeploy/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDeploy
+{
+    internal class Test
+    {
+        public void TestCode()
+        {
+            Console.WriteLine("test Code");
+        }
+    }
+}
